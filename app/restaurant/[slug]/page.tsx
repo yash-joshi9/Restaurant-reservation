7751cb7614b5be4399326570b15f2bfe1037038c
yash joshi <yjoshi.2098@gmail.com>
2023-03-12 17:12:02 -0400

    fix indent issue

--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -5,8 +5,7 @@ export default function RestaurantDetailsPage() {
         {/* NAVBAR */}
         <nav className="bg-white p-2 flex justify-between">
           <a href="" className="font-bold text-gray-700 text-2xl">
-            {" "}
-            OpenTable{" "}
+            OpenTable
           </a>
           <div>
             <div className="flex">
