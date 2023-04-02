abcf88cf36777733a8cee05e0ac0f50c1590fd44
yash joshi <yjoshi.2098@gmail.com>
2023-04-02 18:19:21 -0400

    search query params and search fetch api

--- app/components/SearchBar.tsx
+++ app/components/SearchBar.tsx
@@ -6,19 +6,22 @@ import React, { useState } from "react";
 export default function SearchBar() {
   const [search, setSearch] = useState("");
   const router = useRouter();
+
   return (
     <div className="text-left text-lg py-3 m-auto flex justify-center">
       <input
         className="rounded  mr-3 p-2 w-[450px]"
         type="text"
+        value={search}
         placeholder="State, city or town"
         onChange={(e) => setSearch(e.target.value)}
       />
       <button
         className="rounded bg-red-600 px-9 py-2 text-white"
-        onClick={(e) => {
+        onClick={() => {
           if (search === "apple") return;
-          router.push(`/search`);
+          router.push(`/search?city=${search}`);
+          setSearch("");
         }}
       >
         Let's go
--- app/search/components/RestaurantCard.tsx
+++ app/search/components/RestaurantCard.tsx
@@ -1,24 +1,39 @@
+import { Restaurant } from "@prisma/client";
 import Link from "next/link";
+import { restaurantSearchCard } from "../page";
 
-export default function RestaurantCard() {
+export default function RestaurantCard({
+  restaurant,
+}: {
+  restaurant: restaurantSearchCard;
+}) {
+  const { main_image, name, location, price, cuisine } = restaurant;
   return (
     <div className="border-b flex pb-5">
-      <img
-        src="https://images.otstatic.com/prod1/49153814/2/medium.jpg"
-        alt=""
-        className="w-44 rounded"
-      />
+      <img src={main_image} alt="" className="w-44 rounded" />
       <div className="pl-5">
-        <h2 className="text-3xl">Aiāna Restaurant Collective</h2>
+        <h2 className="text-3xl">{name}</h2>
         <div className="flex items-start">
           <div className="flex mb-2">*****</div>
           <p className="ml-2 text-sm">Awesome</p>
         </div>
         <div className="mb-9">
           <div className="font-light flex text-reg">
-            <p className="mr-4">$$$</p>
-            <p className="mr-4">Mexican</p>
-            <p className="mr-4">Ottawa</p>
+            {price === "CHEAP" ? (
+              <>
+                <span>$$ </span> <span className="text-gray-400 mr-4">$$</span>
+              </>
+            ) : price == "REGULAR" ? (
+              <>
+                <span>$$$ </span> <span className="text-gray-400 mr-4">$</span>
+              </>
+            ) : (
+              <>
+                <span className="mr-4">$$$$ </span>
+              </>
+            )}{" "}
+            <p className="mr-4">{cuisine.name}</p>
+            <p className="mr-4">{location.name}</p>
           </div>
         </div>
         <div className="text-red-600">
